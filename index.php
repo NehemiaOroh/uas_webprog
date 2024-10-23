f72a7d7047615d747b095bce4e97b02fc6ab0572
NehemiaOroh <163231891+NehemiaOroh@users.noreply.github.com>
2024-10-23 19:03:31 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,124 @@
+<? 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<header class="header" style="border: 5px black; border-bottom: 0.1px solid black;">
+    <h1>Unite</h1>
+    <nav class="navigation_">
+        <a href="" style="color: black;">Home</a>
+        <a href="" style="color: black;">Events</a>
+        <a href="" style="color: black;">About Us</a>
+    </nav>
+    <div class="navigation_-buttons">
+        <a href="login.php" class="button">Login</a>
+        <a href="" class="button2">Register</a>
+    </div>
+</header>
+
+
+<div class="second" style="height:90vh;">
+    <div class="text-content">
+        <h1>Music Events <span style="color: #cc308a; font-family: Sans-Bold;">Unite</span> in one place</h1>
+        <p style="padding-bottom: 10px;">Your ultimate destination for the best music events.</p>
+        <a href="" class="button" style="margin-left:5px;">Browse Events</a>
+    </div>
+    <img src="headset.png" class="right-image">
+</div>
+
+<div class="container">
+    <div class="scroll">
+        <div class="RightToLeft">
+            <h1>Unite! Unite! Unite! Unite! Unite! Unite! Unite! Unite! Unite! Unite! Unite! </h1>
+            <h1>Unite! Unite! Unite! Unite! Unite! Unite! Unite! Unite! Unite! Unite! Unite! Unite! </h1>
+        </div>
+    </div>
+</div>
+
+<section class="hero">
+    <div class="left-cards">
+        <div class="card1">            
+            <img src="1.jpeg" alt="Business Mindset Planning">
+        </div>
+        <div class="card2">
+            <img src="2.jpeg" alt="Key Success of Career">
+            
+        </div>
+        <div class="card3">
+            <img src="3.jpeg" alt="Management">
+        </div>
+ 
+    </div>
+    <div class="hero-text">
+        <h1 style="font-family: Sans-Bold">Grab Ur Concert Tickets On <span style="color: #cc308a; font-family: Sans-Bold">Unite!</span></h1>
+        <p>Welcome to Unite! Your ultimate destination for unforgettable music experiences. At Unite, we believe in bringing people together through the power of music. Our platform offers a seamless way to discover and purchase tickets for the hottest concerts and events in your area. With a diverse lineup of artists and genres, there’s something for everyone. Whether you’re a die-hard music fan or just looking for a fun night out, Unite has you covered. </p>
+        <a href="" class="button2" style="margin-left:18px;">Learn More!</a>
+    </div>
+</section>
+
+<div class="container">
+    <div class="scroll">
+        <div class="LeftToRight">
+            <h1>Unite! Unite! Unite! Unite! Unite! Unite! Unite! Unite! Unite! Unite! Unite! </h1>
+            <h1>Unite! Unite! Unite! Unite! Unite! Unite! Unite! Unite! Unite! Unite! Unite! Unite! </h1>
+        </div>
+    </div>
+</div>
+
+<div class="events">
+    <div class="event-text">
+        <h1>Recent <br>Events</h1>
+    </div>
+    <div class="recent_image">
+    <div class="event_item">
+        <img src="nadin.jpeg" alt="nadine">
+        <h2 class="event-description">28 Oktober 2024</h2>
+        <h1 class="event_name">Nadine Amizah Concert</h1>
+    </div>
+    
+    <div class="event_item">
+        <img src="juicyluicy.png" alt="nadine">
+        <h2 class="event-description">28 Oktober 2024</h2>
+        <h1 class="event_name">Juicy Lucy Concert</h1>
+    </div>
+
+    <div class="event_item">
+        <img src="bernadya.jpeg" alt="nadine">
+        <h2 class="event-description">28 Oktober 2024</h2>
+        <h1 class="event_name">Bernadya Concert</h1>
+    </div>
+    <div class="event_item2">
+        <img src="nadin.jpeg" alt="nadine">
+        <h2 class="event-description">28 Oktober 2024</h2>
+        <h1 class="event_name">Rizky Febian & Mahalini Concert</h1>
+    </div>
+    <div class="event_item2">
+        <img src="nadin.jpeg" alt="nadine">
+        <h2 class="event-description">28 Oktober 2024</h2>
+        <h1 class="event_name">Tulus Concert</h1>
+    </div>
+    <div class="event_item2">
+        <img src="nadin.jpeg" alt="nadine">
+        <h2 class="event-description">28 Oktober 2024</h2>
+        <h1 class="event_name">Hivi! Concert</h1>
+    </div>
+
+    
+</div>
+</div>
+
+
+
+
+
+    
+</body>
+</html>
